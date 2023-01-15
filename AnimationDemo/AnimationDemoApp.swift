398459a0c18ce7218ee06c03ada82e74d87f8065
apple <apple@MacBook-Pro.local>
2023-01-15 22:06:14 +0100

    initial commit

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by apple on 15/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
