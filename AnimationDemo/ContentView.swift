ea26703629973a53a9db7c50acc6999396047f81
apple <apple@MacBook-Pro.local>
2023-01-16 08:41:35 +0100

    add spring to animation modifier

--- AnimationDemo/ContentView.swift
+++ AnimationDemo/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rotation: Double = 0.0;
+    @State private var scale: CGFloat = 1;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button(action: {
+            self.rotation = self.rotation == 360 ? 0 : self.rotation + 60;
+            self.scale = self.scale < 2.8 ? self.scale + 0.3 : 1;
+        }){
+            Text("Click me")
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .rotationEffect(.degrees(rotation))
+                .scaleEffect(scale)
+                .animation(.spring(response: 1, dampingFraction: 0.2,blendDuration: 0), value: rotation)
         }
-        .padding()
     }
 }
 
