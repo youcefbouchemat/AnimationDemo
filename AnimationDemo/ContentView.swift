b4de8d6456ecdb1e25d0edca73a4fb3df9aac4fb
apple <apple@MacBook-Pro.local>
2023-01-16 12:24:20 +0100

    use Explicit animation

--- AnimationDemo/ContentView.swift
+++ AnimationDemo/ContentView.swift
@@ -12,16 +12,16 @@ struct ContentView: View {
     @State private var scale: CGFloat = 1;
     
     var body: some View {
-        Button(action: {
+        Button(action: { withAnimation(.linear(duration: 2)){
             self.rotation = self.rotation == 360 ? 0 : self.rotation + 60;
             self.scale = self.scale < 2.8 ? self.scale + 0.3 : 1;
+        }
         }){
             Text("Click me")
                 .foregroundColor(.black)
                 .font(.largeTitle)
                 .rotationEffect(.degrees(rotation))
                 .scaleEffect(scale)
-                .animation(Animation.linear(duration: 1).repeatCount(10,autoreverses: false), value: rotation)
         }
     }
 }
